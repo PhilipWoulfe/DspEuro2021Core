dc361b3869826b0f71317c6dbfcf5193ddc8fd19
Philip Woulfe <PhilipWoulfe@outlook.ie>
2021-06-04 15:11:03 +0100

    #1 Tidy Up Code

--- TodoListService/Models/TodoItem.cs
+++ TodoListService/Models/TodoItem.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,10 +8,19 @@ namespace TodoListService.Models
 {
     public class Todo
     {
+        [JsonProperty(PropertyName = "id")]
         public int Id { get; set; }
 
+        [JsonProperty(PropertyName = "title")]
         public string Title { get; set; }
 
+        [JsonProperty(PropertyName = "owner")]
         public string Owner { get; set; }
+
+        [JsonProperty(PropertyName = "description")]
+        public string Description { get; set; }
+
+        [JsonProperty(PropertyName = "isComplete")]
+        public bool Completed { get; set; }
     }
 }
