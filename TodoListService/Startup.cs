464340a015d44755af03ee7c2b8252b7d37f4251
PhilipWoulfe <philip.woulfe@gmail.com>
2021-06-08 01:37:52 +0100

    Add swagger to API Fixed #9

--- TodoListService/Startup.cs
+++ TodoListService/Startup.cs
@@ -39,12 +39,19 @@ namespace TodoListService
             },
             options => { Configuration.Bind("AzureAdB2C", options); });
 
+            services.AddMvcCore()
+                .AddApiExplorer();
+
             services.AddControllers();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
             if (env.IsDevelopment())
             {
                 // Since IdentityModel version 5.2.1 (or since Microsoft.AspNetCore.Authentication.JwtBearer version 2.2.0),
@@ -52,6 +59,16 @@ namespace TodoListService
                 // For debugging/development purposes, one can enable additional detail in exceptions by setting IdentityModelEventSource.ShowPII to true.
                 // Microsoft.IdentityModel.Logging.IdentityModelEventSource.ShowPII = true;
                 app.UseDeveloperExceptionPage();
+
+                // Enable middleware to serve generated Swagger as a JSON endpoint.
+                app.UseSwagger();
+
+                // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+                // specifying the Swagger JSON endpoint.
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "DSP Euro 2021 V1");
+                });
             }
             else
             {
